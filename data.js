8336cec1d81fa8fb99c50b69b4ccc41c34608c88
MahshadOliaie <mahshadoliaie@gmail.com>
2023-09-02 01:27:58 +0330

    debug pagination

--- data.js
+++ data.js
@@ -7,7 +7,7 @@ let h1 = document.querySelector(".h1");
 let searchInput = document.querySelector(".search");
 let pages = document.querySelector(".pages");
 let pageNumbers = [];
-
+let likedBooksId =[]
 let likedBooks = [];
 const BOOKS = [
   {
@@ -188,20 +188,22 @@ const BOOKS = [
 
 
 function like(id) {
-  if (likedBooks.includes(id)) {
-    likedBooks.splice(likedBooks.indexOf(id), 1);
+  if (likedBooksId.includes(id)) {
+    likedBooksId.splice(likedBooksId.indexOf(id), 1);
   } else
-    likedBooks.push(id);
-    pagination(page)
+    likedBooksId.push(id);
+
+    likedBooks = likedBooksId.map(item => {
+      return item = BOOKS.find(book => book.id === item)
+    })
+
+  pagination(page, BOOKS);
 }
 
 
 
 function showLikedBooksFN() {
-  likedBooks = likedBooks.map(item => {
-    return item = BOOKS.find(book => book.id === item)
-  })
-  render(likedBooks);
+  pagination(1, likedBooks);
   let hearts = [...document.querySelectorAll(".like")];
   for (const heart of hearts) {
     heart.classList.add("dnone")
@@ -214,11 +216,11 @@ function showLikedBooksFN() {
 }
 
 
-
+let res = []
 function search() {
   let value = searchInput.value;
-  let res = BOOKS.filter(item => item.title.includes(value) || item.author.includes(value));
-  render(res);
+  res = BOOKS.filter(item => item.title.includes(value) || item.author.includes(value));
+  pagination(1, res)
 }
 
 
@@ -249,29 +251,33 @@ function showDetails(id) {
 
 
 let num = 4;
-let pageCount = Math.ceil(BOOKS.length / num);
+let pageCount;
 let showItems = [];
 let page = 1;
 
-function pagination(p) {
+function pagination(p, data) {
+  console.log(likedBooks)
+  pageCount = Math.ceil(data.length / num);
   page = p;
   if (p == pageCount)
-    showItems = BOOKS.slice((p - 1) * num);
+    showItems = data.slice((p - 1) * num);
 
   else
-    showItems = BOOKS.slice((p - 1) * num, (p * num));
+    showItems = data.slice((p - 1) * num, (p * num));
 
   render(showItems);
-  pagesFN();
+  pagesFN(data);
 
-  pageNumbers[p - 1].style.backgroundColor = "rgb(129, 129, 129)";
+  if (pageNumbers.length > 0)
+    pageNumbers[p - 1].style.backgroundColor = "rgb(129, 129, 129)";
 }
 
 
-function pagesFN() {
+function pagesFN(data) {
+  console.log(data)
   pages.innerHTML = "";
   for (let i = 1; i <= pageCount; i++) {
-    pages.innerHTML += `<p onclick="pagination(${i})" class="pagechoose">${i}</p>`
+    pages.innerHTML += `<p onclick="pagination(${i},${(data == BOOKS) ? "BOOKS" : (data == likedBooks) ? "likedBooks" : "res"})" class="pagechoose">${i}</p>`
   }
   pageNumbers = document.querySelectorAll(".pagechoose")
 }
@@ -293,9 +299,9 @@ function render(data) {
         <div><h2 class="title">${title}</h2>
         <p class="author">${author}</p></div>
         <div class="like" onclick='like(${id})'>
-    <svg class="svg  ${(likedBooks.includes(id)) ? "red" : ""} " xmlns="http://www.w3.org/2000/svg" height="1.5em"
+    <svg class="svg  ${(likedBooksId.includes(id)) ? "red" : ""} " xmlns="http://www.w3.org/2000/svg" height="1.5em"
     viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-   ${(likedBooks.includes(id)) ? `<path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/>` : ` <path d="M225.8 468.2l-2.5-2.3L48.1 303.2C17.4 274.7 0 234.7 0 192.8v-3.3c0-70.4 50-130.8 119.2-144C158.6 37.9 198.9 47 231 69.6c9 6.4 17.4 13.8 25 22.3c4.2-4.8 8.7-9.2 13.5-13.3c3.7-3.2 7.5-6.2 11.5-9c0 0 0 0 0 0C313.1 47 353.4 37.9 392.8 45.4C462 58.6 512 119.1 512 189.5v3.3c0 41.9-17.4 81.9-48.1 110.4L288.7 465.9l-2.5 2.3c-8.2 7.6-19 11.9-30.2 11.9s-22-4.2-30.2-11.9zM239.1 145c-.4-.3-.7-.7-1-1.1l-17.8-20c0 0-.1-.1-.1-.1c0 0 0 0 0 0c-23.1-25.9-58-37.7-92-31.2C81.6 101.5 48 142.1 48 189.5v3.3c0 28.5 11.9 55.8 32.8 75.2L256 430.7 431.2 268c20.9-19.4 32.8-46.7 32.8-75.2v-3.3c0-47.3-33.6-88-80.1-96.9c-34-6.5-69 5.4-92 31.2c0 0 0 0-.1 .1s0 0-.1 .1l-17.8 20c-.3 .4-.7 .7-1 1.1c-4.5 4.5-10.6 7-16.9 7s-12.4-2.5-16.9-7z" />`}
+   ${(likedBooksId.includes(id)) ? `<path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/>` : ` <path d="M225.8 468.2l-2.5-2.3L48.1 303.2C17.4 274.7 0 234.7 0 192.8v-3.3c0-70.4 50-130.8 119.2-144C158.6 37.9 198.9 47 231 69.6c9 6.4 17.4 13.8 25 22.3c4.2-4.8 8.7-9.2 13.5-13.3c3.7-3.2 7.5-6.2 11.5-9c0 0 0 0 0 0C313.1 47 353.4 37.9 392.8 45.4C462 58.6 512 119.1 512 189.5v3.3c0 41.9-17.4 81.9-48.1 110.4L288.7 465.9l-2.5 2.3c-8.2 7.6-19 11.9-30.2 11.9s-22-4.2-30.2-11.9zM239.1 145c-.4-.3-.7-.7-1-1.1l-17.8-20c0 0-.1-.1-.1-.1c0 0 0 0 0 0c-23.1-25.9-58-37.7-92-31.2C81.6 101.5 48 142.1 48 189.5v3.3c0 28.5 11.9 55.8 32.8 75.2L256 430.7 431.2 268c20.9-19.4 32.8-46.7 32.8-75.2v-3.3c0-47.3-33.6-88-80.1-96.9c-34-6.5-69 5.4-92 31.2c0 0 0 0-.1 .1s0 0-.1 .1l-17.8 20c-.3 .4-.7 .7-1 1.1c-4.5 4.5-10.6 7-16.9 7s-12.4-2.5-16.9-7z" />`}
 </svg>
 </div>
     </div>
@@ -306,7 +312,7 @@ function render(data) {
 
 
 
-  
+
 
 }
 
@@ -315,12 +321,13 @@ function render(data) {
 
 
 home.addEventListener("click", function () {
-  render(BOOKS)
+  pagination(page, BOOKS)
 });
 
 window.addEventListener("load", function () {
-  pagination(page)
+  pagination(page, BOOKS)
 })
+
 showLikedBooks.addEventListener("click", showLikedBooksFN);
 
 searchInput.addEventListener("keyup", search);
